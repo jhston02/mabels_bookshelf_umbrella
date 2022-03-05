d51168cc7ad89abc83730a759daca7365e03cd37
Coleslaw <coleslaw.john@gmail.com>
2022-03-05 18:59:14 -0500

    adds the hunky boy

---
+++ apps/mabels_bookshelf/lib/mabels_bookshelf/connections/spear_supervisor.ex
@@ -0,0 +1,26 @@
+defmodule MabelsBookshelf.Connections.SpearSupervisor do
+  use Supervisor
+
+  defp poolboy_config do
+    [
+      name: {:local, :spear_supervisor},
+      worker_module: Spear.Connection,
+      size: 10,
+      max_overflow: 2
+    ]
+  end
+
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      :poolboy.child_spec(:spear_supervisor, poolboy_config())
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
--- apps/mabels_bookshelf/lib/mabels_bookshelf/repo.ex
+++ apps/mabels_bookshelf/lib/mabels_bookshelf/repo.ex
@@ -1,3 +1,83 @@
 defmodule MabelsBookshelf.Repo do
+  alias MabelsBookshelf.Behaviors.Event
 
+  def insert(module, entity, stream_name) do
+    run_on_pool(&(insert(&1, module, entity, stream_name)))
+  end
+
+  defp insert(connection, module, entity, stream_name) do
+    get_events_from_aggregate(module, entity)
+    |> events_to_spear_events()
+    |> Spear.append(connection, stream_name, expect: :empty)
+
+    clear_pending_events(module, entity)
+  end
+
+  def update(module, entity, stream_name) do
+    run_on_pool(&(update(&1, module, entity, stream_name)))
+  end
+
+  defp update(connection, module, entity, stream_name) do
+    events = get_events_from_aggregate(module, entity)
+    revision = calculate_revision(module, events, entity)
+
+    events
+    |> events_to_spear_events()
+    |> Spear.append(connection, stream_name, expect: revision)
+
+    clear_pending_events(module, entity)
+  end
+
+  def get(module, stream_name) do
+    run_on_pool(&(get(&1, module, stream_name)))
+  end
+
+  defp get(connection, module, stream_name) do
+    Spear.stream!(connection, stream_name)
+    |> apply_events(module)
+  end
+
+  defp apply_events(events, module) do
+    spear_events_to_events(events)
+    |> Enum.reduce(struct(module), fn event, entity ->
+      apply(module, :apply_event, [entity, event])
+    end)
+  end
+
+  defp calculate_revision(module, events, entity) do
+    version = apply(module, :get_version, [entity])
+    version - length(events)
+  end
+
+  defp clear_pending_events(module, entity) do
+    apply(module, :clear_pending_events, [entity])
+  end
+
+  defp get_events_from_aggregate(module, entity) do
+    apply(module, :get_pending_events, [entity])
+  end
+
+  defp events_to_spear_events(events) do
+    events
+    |> Enum.map(&event_to_spear_event/1)
+  end
+
+  defp event_to_spear_event(event) do
+    Spear.Event.new(event.type, event.body)
+  end
+
+  defp spear_events_to_events(events) do
+    events
+    |> Enum.map(&spear_event_to_event/1)
+  end
+
+  defp spear_event_to_event(event) do
+    Event.new(event.type, event.body)
+  end
+
+  defp run_on_pool(action) do
+    :poolboy.transaction(:spear_supervisor, fn pid ->
+      action.(pid)
+    end)
+  end
 end
