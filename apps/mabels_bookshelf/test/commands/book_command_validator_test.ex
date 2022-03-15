96625d4727d403fcd4b8e75ee3580293b435ea71
Coleslaw <coleslaw.john@gmail.com>
2022-03-14 20:55:48 -0400

    More validations

---
+++ apps/mabels_bookshelf/test/commands/book_command_validator_test.ex
@@ -0,0 +1,12 @@
+defmodule BookCommandValidatorTest do
+  use ExUnit.Case
+
+  alias MabelsBookshelf.Commands.Book.CommandValidator
+
+  describe "Create book command" do
+    test "Given valid create_book_command, when validated, passes validation" do
+
+    end
+
+  end
+end
